0d68762e60731358b48698c1deb18148b3d48ceb
Kyle <kjfossman@gmail.com>
2021-02-24 12:23:10 -0900

    Refactored the random_clues method in the CLI class

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,4 +1,6 @@
 require 'pry'
+require 'awesome_print'
+
 
 class CLI
     attr_accessor :board
@@ -10,46 +12,35 @@ class CLI
         greeting
     end
 
-    
-    # how to get random clues in this method
-    def test 
-        @board = @board.map do |x|
-            x = 'pizza'
-        end
-        # binding.pry
-    end
-
     def greeting
         puts "Hello are there one or two players playing today?"
         play_board
     end
 
     def play_board
-        puts "      (1)#{@board[0].category.title}  $(#{@board[0].value})     |      (2)#{@board[1].category.title}  $(#{@board[1].value})     |      (3)#{@board[2].category.title}  $(#{@board[2].value})  "
-        puts ""
-        puts "      (4)#{@board[3].category.title}  $(#{@board[3].value})     |      (5)#{@board[4].category.title}  $(#{@board[4].value})     |      (6)#{@board[5].category.title}  $(#{@board[5].value})  "
-        puts ""
-        puts "      (7)#{@board[6].category.title}  $(#{@board[6].value})     |      (8)#{@board[7].category.title}  $(#{@board[7].value})     |      (9)#{@board[8].category.title}  $(#{@board[8].value})  "
-        puts ""
-        puts "      (10)#{@board[9].category.title}  $(#{@board[9].value})     |      (11)#{@board[10].category.title}  $(#{@board[10].value})     |      (12)#{@board[11].category.title}  $(#{@board[11].value})  "
+        display = []
+        @board.each.with_index(1) do |x, index|
+            display << "#{index}: #{x.category.title} $#{x.value}"
+            display << ""
+        end 
+        puts display
+        binding.pry
     end
 
     def random_clues
-        group = []
-        id = []
-        12.times do 
-            id << Clue.all.sample.id
-        end
-        if id.uniq == id
-        object = Clue.all.select do |x| 
-           if id.include?(x.id)
-            group << x
-           end
-        end 
-    else 
-        random_clues
-    end
-        @board = group
+        @board = Clue.all.sample(12)
+        # id = []
+        #     12.times do 
+        #         id << Clue.all.sample.id
+        #     end
+        # if id.uniq == id
+        #     object = Clue.all.select do |x| 
+        #     id.include?(x.id)
+        #     end 
+        # else 
+        #     random_clues
+        # end
+        # @board = object
     end
 
     def board_full
@@ -57,3 +48,13 @@ class CLI
     end
 
 end
+
+
+
+        # # ap       "(1)#{@board[0].category.title}  $(#{@board[0].value})     |      (2)#{@board[1].category.title}  $(#{@board[1].value})     |      (3)#{@board[2].category.title}  $(#{@board[2].value})  "
+        # puts ""
+        # puts "      (4)#{@board[3].category.title}  $(#{@board[3].value})     |      (5)#{@board[4].category.title}  $(#{@board[4].value})     |      (6)#{@board[5].category.title}  $(#{@board[5].value})  "
+        # puts ""
+        # puts "      (7)#{@board[6].category.title}  $(#{@board[6].value})     |      (8)#{@board[7].category.title}  $(#{@board[7].value})     |      (9)#{@board[8].category.title}  $(#{@board[8].value})  "
+        # puts ""
+        # puts "      (10)#{@board[9].category.title}  $(#{@board[9].value})     |      (11)#{@board[10].category.title}  $(#{@board[10].value})     |      (12)#{@board[11].category.title}  $(#{@board[11].value})  "
